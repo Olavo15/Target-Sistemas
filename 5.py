b162dc9d8094233864143193c56b68cc3b569371
Olavo15 <olavoregis169@gmail.com>
2024-09-03 20:48:40 -0300

    end

---
+++ 5.py
@@ -0,0 +1,18 @@
+
+string = input("")
+
+
+def inverter_string(s):
+
+    string_invertida = ""
+    
+
+    for i in range(len(s) - 1, -1, -1):
+        string_invertida += s[i]
+    
+    return string_invertida
+
+
+string_invertida = inverter_string(string)
+print("String original: ", string)
+print("String invertida: ", string_invertida)
